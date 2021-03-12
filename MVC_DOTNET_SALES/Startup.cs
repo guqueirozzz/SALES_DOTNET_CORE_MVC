b7fb0b52b890ab90b4b5e0f677011099e7401071
Gustavo Queiroz <guaraujoo@hotmail.com>
2021-03-12 18:47:17 -0300

    Migration MySQL

--- MVC_DOTNET_SALES/Startup.cs
+++ MVC_DOTNET_SALES/Startup.cs
@@ -36,8 +36,10 @@ namespace MVC_DOTNET_SALES
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<MVC_DOTNET_SALESContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("MVC_DOTNET_SALESContext")));
+            services.AddDbContext<MVC_DOTNET_SALESContext>
+                (options => options.UseMySql(Configuration.GetConnectionString("MVC_DOTNET_SALESContext"), builder => builder.MigrationsAssembly("MVC_DOTNET_SALES")));
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
